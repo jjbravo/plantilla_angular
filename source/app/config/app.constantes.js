4dc1b02d0e8713b402b9ec966f161c63954b91de
bratc <bramasterweb@gmail.com>
2016-03-05 10:11:04 -0500

    Mi primer test

---
+++ source/app/config/app.constantes.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('app.config', [
+    ]).constant('BASEURL','http://10.72.255.173:8080/Portafolio-Backend/webresources', '');
+
+    //En este módulo se pueden declarar y asignar todas las constantes
+    //que se usarán en la aplicación.
+
+})();
